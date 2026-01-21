97b29b59d5c859bc0925bee5f0ce696499cf6440
MandaStrong01 <woolleya129@gmail.com>
2026-01-21 14:37:25 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,9 @@
-src/App.tsx
-src/App.css
-public/background.mp4
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  }
+});
