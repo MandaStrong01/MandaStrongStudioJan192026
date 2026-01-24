f5807dd046725f953fcb9aebe9da2eb1d5c5562e
MandaStrong01 <woolleya129@gmail.com>
2026-01-24 10:30:46 -0500

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-const root = document.getElementById('root')!;
-ReactDOM.createRoot(root).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
