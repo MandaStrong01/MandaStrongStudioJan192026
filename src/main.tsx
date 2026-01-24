7a9f077287b41bcaeef2d1552f682af15d32928f
MandaStrong01 <woolleya129@gmail.com>
2026-01-24 10:34:20 -0500

    Updated App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client';
+import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = document.getElementById('root')!;
+ReactDOM.createRoot(root).render(
+  <AuthProvider>
     <App />
-  </StrictMode>,
+  </AuthProvider>
 );
